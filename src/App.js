20d9ac0a0501992223dee2b6514fa9539ef5534b
Vũ Hoan Abc <continuesorhalt@gmail.com>
2021-05-06 14:55:08 +0700

    test login fb

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import { useEffect, useState } from "react";
 import FormCreate from "./component/FormTask";
 import TodoList from "./component/TodoList";
 import { compareTask } from "./ult/common";
+import firebase from "firebase/app";
 import "./App.css";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2yvoTIySraWdSbxmwsJLfgQ_XPj-JdH8",
+  authDomain: "atastay.firebaseapp.com",
+  databaseURL: "https://atastay.firebaseio.com",
+  projectId: "atastay",
+  storageBucket: "atastay.appspot.com",
+  messagingSenderId: "943426852786",
+  appId: "1:943426852786:web:e224c128d2bfb3121eb6ab",
+  measurementId: "G-B1GGTF443Z",
+};
+// Initialize Firebase
+try {
+  firebase.initializeApp(firebaseConfig);
+} catch (error) {}
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -44,12 +61,59 @@ function App() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
+  const login = (type) => () => {
+    let provider;
+    switch (type) {
+      case "f":
+        provider = new firebase.auth.FacebookAuthProvider();
+        break;
+      case "g":
+        provider = new firebase.auth.GoogleAuthProvider();
+        break;
+
+      default:
+        break;
+    }
+
+    firebase.auth().signInWithRedirect(provider);
+  };
+
+  useEffect(() => {
+    firebase
+      .auth()
+      .getRedirectResult()
+      .then((result) => {
+        console.log("😡coh / file: App.js / line 87 / result", result);
+        if (result.credential) {
+          console.log(`1`, 1);
+        }
+      })
+      .catch((error) => {
+        console.log("😡coh / file: App.js / line 94 / error", error);
+      });
+  }, []);
+
   return (
     <div className="container">
       <div className="left-content">
         <div className="p-50">
           <p className="text-header text-center">New Task</p>
           <FormCreate value={{}} onChange={onCreate} />
+          <br />
+          <div className="flex gap-20">
+            <button
+              onClick={login("f")}
+              className="btn bg-blue w-full text-white"
+            >
+              loginFb
+            </button>
+            <button
+              onClick={login("g")}
+              className="btn bg-red w-full text-white"
+            >
+              loginGG
+            </button>
+          </div>
         </div>
       </div>
 
